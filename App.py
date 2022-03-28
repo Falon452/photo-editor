de695c1c8eb0f08bfd48ab14462f0a1d4f809c46
adabd-pl <72196988+adabd-pl@users.noreply.github.com>
2022-03-28 23:51:50 +0200

    Add basic photo effects

--- App.py
+++ App.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog
-from PIL import Image, ImageTk
+from tkinter import filedialog,Scale, HORIZONTAL
+from tkinter.colorchooser import askcolor
+from PIL import Image, ImageTk, ImageEnhance
+import cv2 
+from PIL import ImageFilter, ImageOps
+from PIL.ImageFilter import (
+    ModeFilter)
 
 
 class MenuBar(ttk.Frame):
@@ -22,6 +27,11 @@ class MenuBar(ttk.Frame):
             label='Open Image',
             command=parent.toolbar.file_dialog,
         )
+        
+        file_menu.add_command(
+            label='Save Image',
+            command=parent.imageframe.save_image
+        )
 
         file_menu.add_separator()
 
@@ -29,6 +39,46 @@ class MenuBar(ttk.Frame):
             label='Exit',
             command=parent.destroy,
         )
+
+class ImageEffectsBar(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        self.button = ttk.Button(self, text="Paint Effect", command = self.parent.imageframe.paint_effect) 
+        self.button.pack(side="left")
+
+        self.button = ttk.Button(self, text="Invert Effect", command = self.parent.imageframe.invert_effect)
+        self.button.pack(side="left")
+
+        self.button = ttk.Button(self, text="Solarize Effect", command = self.parent.imageframe.solarize_effect)
+        self.button.pack(side="left")
+
+        self.button = ttk.Button(self,text='Filter Color',command=self.change_color)
+        self.button.pack(side="left" , expand=True)
+
+
+        self.slider_label = ttk.Label(self,text='Brightness: ')
+        self.slider_label.pack(side="left" , expand=True)
+
+        self.slider = ttk.Scale(self, from_=0, to=2 ,orient=HORIZONTAL ,value=1)
+        self.slider.bind("<ButtonRelease-1>", self.change_brightness)
+        self.slider.pack(side="left" , expand=True)
+        
+    def change_color(self):
+        colors = askcolor(title="Color Chooser")
+        self.parent.imageframe.color_effect(colors[0])
+    
+    def change_brightness(self, new_value):
+        new_value = self.slider.get()
+        self.parent.imageframe.brightness_effect(new_value)
+
+
+    
+    
+ 
 
 
 class ToolBar(ttk.Frame):
@@ -55,20 +105,77 @@ class ImageFrame(ttk.Frame):
         self.parent = parent
         self.image_label = ttk.Label(self)
         self.image_label.pack()
+        
+    
 
     def set_image(self, filepath):
         self.image_label.pack_forget()  # otherwise images are stacking up
         PIL_image = Image.open(filepath)
         tk_image = tk.PhotoImage(filepath)
-
+        #self.opencv_img = cv2.imread(filepath)     #not working
+        #self.opencv_img = cv2.cvtColor(self.opencv_img, cv2.COLOR_BGR2RGB)
         img = Image.open(filepath)
+        self.img = img
+        img=img.resize((550,350))
+       
         photo = ImageTk.PhotoImage(img)
 
         self.image_label = ttk.Label(self, image=photo)
         self.image_label.image = photo  # this is necessary
-
+        self.enhancer = ImageEnhance.Brightness(self.img)
+        
         self.image_label.pack()
 
+    def save_image(self):
+        filename = filedialog.asksaveasfile(mode='w', defaultextension=".jpg")
+        if not filename:
+            return
+        self.img.save(filename)    
+
+    def change_img(self, res):
+        res=res.resize((550,350))
+        photo1 = ImageTk.PhotoImage(res)
+        self.image_label = ttk.Label(self, image=photo1)
+        self.image_label.image = photo1
+        self.img=res
+        self.img_without_effects=res
+        self.image_label.pack()
+        
+        
+
+    def paint_effect(self):
+        #self.opencv_img = cv2.xphoto.oilPainting(self.opencv_img, 7, 1)
+        res = self.img.filter(ModeFilter(size=7))  
+        self.image_label.pack_forget() 
+        self.change_img(res)
+        self.enhancer = ImageEnhance.Brightness(self.img)  
+    
+    def invert_effect(self): 
+        res = ImageOps.invert(self.img)
+        self.image_label.pack_forget() 
+        self.change_img(res)
+        self.enhancer = ImageEnhance.Brightness(self.img)
+    
+    def solarize_effect(self):
+        res = ImageOps.solarize(self.img, threshold=50)
+        self.image_label.pack_forget()
+        self.change_img(res)
+        self.enhancer = ImageEnhance.Brightness(self.img)
+
+    def color_effect(self ,color):
+        effect_image = self.img.convert('L')
+        res = ImageOps.colorize(effect_image,black=color, white="white")
+        self.image_label.pack_forget()
+        self.change_img(res)
+        self.enhancer = ImageEnhance.Brightness(self.img)
+
+    def brightness_effect(self,new_value):
+        res = self.enhancer.enhance(new_value)
+        self.image_label.pack_forget()
+        self.change_img(res)
+        
+
+        
 
 class MainApplication(tk.Tk):
     def __init__(self):
@@ -93,12 +200,15 @@ class MainApplication(tk.Tk):
         self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
         self.toolbar = ToolBar(self)
-        self.menubar = MenuBar(self)
         self.imageframe = ImageFrame(self)
+        self.menubar = MenuBar(self)
+        self.effectsbar = ImageEffectsBar(self)
 
         self.imageframe.pack(side="bottom", fill="x", expand=True)
-        self.toolbar.pack(side="top", fill="x")
-
+        self.toolbar.pack(side="left", fill="x")
+        self.menubar.pack(side="top", fill="x")
+        self.effectsbar.pack(side="left", fill="x")
+        
 
 if __name__ == '__main__':
     app = MainApplication()
